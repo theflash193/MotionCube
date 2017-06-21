734f02fc33d9bc3caefc5c69388992bd7bafbab4
Gordon RASS-KWASI <grass-kw@e2r1p7.42.fr>
2017-06-21 17:19:05 +0200

    elasticity

--- MotionCube/ViewController.swift
+++ MotionCube/ViewController.swift
@@ -12,28 +12,35 @@ class ViewController: UIViewController {
     var factory = FactoryForm()
     var dynamic = UIDynamicAnimator()
     let gravity = UIGravityBehavior()
-//    var boundries = UICollisionBehavior?
     let collision = UICollisionBehavior()
+    let elasticity = UIDynamicItemBehavior()
     
     @IBAction func TapAction(_ sender: UITapGestureRecognizer) {
         let square = factory.createAlea(coord: sender.location(in: self.view))
         self.view.addSubview(square)
         let boundries = UICollisionBehavior(items: [square])
         boundries.translatesReferenceBoundsIntoBoundary = true
+        
+        
         gravity.addItem(square)
         collision.addItem(square)
         boundries.addItem(square)
+        
+        
+        elasticity.addItem(square)
         dynamic.addBehavior(boundries)
         dynamic.addBehavior(collision)
+        dynamic.addBehavior(elasticity)
+
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         dynamic = UIDynamicAnimator(referenceView: self.view)
-        gravity.magnitude = 3
-        
-        dynamic.addBehavior(gravity)
 
+        gravity.magnitude = 3
+        elasticity.elasticity = 1
+        dynamic.addBehavior(gravity)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
