becab3cf56701b756faa126417fed0718d6ca922
Gordon RASS-KWASI <grass-kw@e2r1p7.42.fr>
2017-06-21 16:54:37 +0200

    collision entre objet

--- MotionCube/ViewController.swift
+++ MotionCube/ViewController.swift
@@ -10,20 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     var factory = FactoryForm()
-    let dynamic = UIDynamicAnimator()
+    var dynamic = UIDynamicAnimator()
+    let gravity = UIGravityBehavior()
+//    var boundries = UICollisionBehavior?
+    let collision = UICollisionBehavior()
     
     @IBAction func TapAction(_ sender: UITapGestureRecognizer) {
         let square = factory.createAlea(coord: sender.location(in: self.view))
         self.view.addSubview(square)
-        
-        let gravity = UIGravityBehavior()
-        gravity.magnitude = 3
-        dynamic.addBehavior(gravity)
+        let boundries = UICollisionBehavior(items: [square])
+        boundries.translatesReferenceBoundsIntoBoundary = true
         gravity.addItem(square)
+        collision.addItem(square)
+        boundries.addItem(square)
+        dynamic.addBehavior(boundries)
+        dynamic.addBehavior(collision)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        dynamic = UIDynamicAnimator(referenceView: self.view)
+        gravity.magnitude = 3
+        
+        dynamic.addBehavior(gravity)
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
